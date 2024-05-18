c2805c24dafd3bcd773ab4b439401c682046e216
Saipronoti <saipronotimukherjee27@gmail.com>
2024-05-18 12:37:29 +0530

    added DAO methods

--- src/main/java/com/questionpro/assessment/dao/AddItemsDaoImpl.java
+++ src/main/java/com/questionpro/assessment/dao/AddItemsDaoImpl.java
@@ -1,7 +1,34 @@
 package com.questionpro.assessment.dao;
 
+import com.questionpro.assessment.entity.GroceryItem;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AddItemsDaoImpl implements ItemDAO{
+    @Override
+    public List<GroceryItem> findAll() {
+        return null;
+    }
+
+    @Override
+    public GroceryItem findById(int theId) {
+        return null;
+    }
+
+    @Override
+    public GroceryItem save(GroceryItem theEmployee) {
+        return null;
+    }
+
+    @Override
+    public GroceryItem update(GroceryItem theEmployee) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
 }
--- src/main/java/com/questionpro/assessment/dao/ItemDAO.java
+++ src/main/java/com/questionpro/assessment/dao/ItemDAO.java
@@ -1,4 +1,14 @@
 package com.questionpro.assessment.dao;
 
+import com.questionpro.assessment.entity.GroceryItem;
+
+import java.util.List;
+
 public interface ItemDAO {
+    List<GroceryItem> findAll();
+    GroceryItem findById(int theId);
+    GroceryItem save(GroceryItem theEmployee);
+    GroceryItem update(GroceryItem theEmployee);
+
+    void deleteById(int theId);
 }
