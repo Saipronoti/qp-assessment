b7fb81c4d738ba242ebbb3402b664dd231a0f7e3
Saipronoti <saipronotimukherjee27@gmail.com>
2024-05-22 13:36:43 +0530

    authorizing requests on basis of url based on role

--- src/main/java/com/questionpro/assessment/security/RolesSecurityConfig.java
+++ src/main/java/com/questionpro/assessment/security/RolesSecurityConfig.java
@@ -1,7 +1,26 @@
 package com.questionpro.assessment.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class RolesSecurityConfig {
+
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+    httpSecurity.authorizeHttpRequests(configure->
+                configure.requestMatchers(HttpMethod.POST,"/questionpro/items").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/questionpro/items").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/questionpro/items").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/questionpro/items").hasRole("ADMIN")
+    );
+    httpSecurity.httpBasic(Customizer.withDefaults());
+    return httpSecurity.build();
 }
+}
+
