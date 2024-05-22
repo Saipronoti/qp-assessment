7014b7373d1ca8d53ef156790ceae8867c6d8b3d
Saipronoti <saipronotimukherjee27@gmail.com>
2024-05-22 14:10:50 +0530

    adding support for hibernate to query with DB.

--- src/main/java/com/questionpro/assessment/entity/GroceryItem.java
+++ src/main/java/com/questionpro/assessment/entity/GroceryItem.java
@@ -1,9 +1,19 @@
 package com.questionpro.assessment.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="item")
 public class GroceryItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private String groceryItemId;
+    @Column(name="item_name")
     private String name;
+    @Column(name="item_price")
     private double price;
+    @Column(name="item_count")
     private int availableItemCount;
 
     public int getAvailableCount() {
